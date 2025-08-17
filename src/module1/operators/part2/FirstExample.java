49a39900262dc6cf6d862bfc0d405437bdbf9c6c
Guillermo Chang <guillermo.chang87@gmail.com>
2025-08-16 23:50:23 -0600

    modulus operator

--- src/module1/operators/part2/FirstExample.java
+++ src/module1/operators/part2/FirstExample.java
@@ -3,6 +3,14 @@ package module1.operators.part2;
 public class FirstExample {
     public static void main(String[] args) {
 
+        int[] ia = { 1, 2, 3, 4 };
+
+        int idx = (int) (Math.random() * 101) - 50;
+
+        System.out.println(ia[idx / ia.length]);
+
+        // Sometimes : ArrayIndexOutOfBoundsException
+
     }
 
 }
